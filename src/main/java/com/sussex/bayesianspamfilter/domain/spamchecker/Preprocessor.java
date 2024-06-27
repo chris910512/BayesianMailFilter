c9cc6d221864fedfc57b5797140f130a345f77cd
Chris Park <dev.chris.kr@gmail.com>
2024-06-27 09:21:42 +0100

    Add words not to save in Entity

--- src/main/java/com/sussex/bayesianspamfilter/domain/spamchecker/Preprocessor.java
+++ src/main/java/com/sussex/bayesianspamfilter/domain/spamchecker/Preprocessor.java
@@ -4,11 +4,17 @@ import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class Preprocessor {
+    private static final List<String> STOP_WORDS = Arrays.asList("a", "the", "is", "it", "this", "and", "in", "to", "of");
+
     public List<String> preprocess(String content) {
         content = content.toLowerCase().replaceAll("[^a-z ]", "");
-        return Arrays.asList(content.split("\\s+"));
+        List<String> words = Arrays.asList(content.split("\\s+"));
+        return words.stream()
+                .filter(word -> !STOP_WORDS.contains(word))
+                .collect(Collectors.toList());
     }
 }
